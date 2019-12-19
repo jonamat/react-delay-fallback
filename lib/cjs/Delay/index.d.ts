0f18af4d65c8d8b9c7f3e79c8120524ae1c6c1d2
jonamat <jo.mataloni@gmail.com>
2019-12-19 11:00:28 +0100

    Prepublish - Linting, tests & build

---
+++ lib/cjs/Delay/index.d.ts
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from 'react';
+interface DelayProps {
+    /** Render delay. Default `0` */
+    timeout?: number;
+    /** Component to render instead of children while waiting.*/
+    fallback?: JSX.Element;
+    /** Function to trigger on render. */
+    onRender?: (...args: Array<unknown>) => unknown;
+    children?: ReactNode;
+}
+/** Delay children components. See [Docs](https://github.com/jonamat/react-delay-fallback). */
+declare const Delay: FC<DelayProps>;
+export default Delay;
